f9edb202503f57e6c1ff7bcdf311602eaf7e62dc
Murilonerdx <mu-silva@outlook.com>
2021-06-16 13:32:14 -0300

    Criando projeto

---
+++ src/test/java/com/digitalinnovation/beerapi/BeerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.beerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
